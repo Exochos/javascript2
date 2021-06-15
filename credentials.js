7a3b56a4392187f9855d3057632a036ba4dcac1c
Exochos <35015441+Exochos@users.noreply.github.com>
2021-06-14 21:46:20 -0700

    Final Update

--- credentials.js
+++ credentials.js
@@ -1,2 +1,2 @@
 module.exports = 'mongodb+srv://jeremyward:00wardjeremy001@cluster0.zh2uj.mongodb.net/doggos01?retryWrites=true&w=majority';
-//module.exports = 'mongodb://localhost:27017/';
+// module.exports = 'mongodb://localhost:27017/';
